f028b503d287f1afecae68395ff0de5b1b3d3b8b
Loyce <loyce.kalume22@students.dkut.ac.ke>
2025-04-10 17:14:48 +0300

    corrected sidebar ..

--- src/app/admin/user-management/user-management.component.ts
+++ src/app/admin/user-management/user-management.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-user-management',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './user-management.component.html',
   styleUrl: './user-management.component.css'
 })
